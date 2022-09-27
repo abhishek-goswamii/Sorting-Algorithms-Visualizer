bd82226c231e7b7d69368737628670a6ecf6228e
Abhishek Goswami <abi.xa12@gmail.com>
2022-09-27 13:38:57 +0530

    ico

--- index.js
+++ index.js
@@ -1,28 +1,37 @@
-var array_size_slider = document.getElementById('array-size-slider'), array_size = array_size_slider.value;
-var generate_new_array_btn = document.getElementById("generate-array-btn")
-var array_speed_slider = document.getElementById("array-speed-slider")
+let array_size_slider = document.getElementById('array-size-slider'), array_size = array_size_slider.value;
+let generate_new_array_btn = document.getElementById("generate-array-btn")
+let array_speed_slider = document.getElementById("array-speed-slider")
 
-var algo_all_btns = document.querySelectorAll(".algo-div button")
+let algo_all_btns = document.querySelectorAll(".algo-div button")
 
-var div_sizes = []
-var divs = []
+let div_height = []
+let divs = []
 
-var margin_size
+let margin_size
 
-var container = document.getElementById("container")
+let container = document.getElementById("container")
 
 container.style = "flex-direction:row"
 
-generate_new_array_btn.addEventListener('click',generateNewArray)
+generate_new_array_btn.addEventListener("click",generateNewArray)
+
 array_size_slider.addEventListener('input',updateArraySize)
 
 function generateNewArray() {
+
     container.innerHTML = ""
-    div_sizes[i] = Math.floor(Math.random() * 0.5 * (inp_as.max - inp_as.min)) + 10;
-    divs[i] = document.createElement("div");
-    cont.appendChild(divs[i]);
+    //making container div empty
+    
+    div_height[i] = Math.floor(Math.random() * (array_size_slider.max - array_size_slider.min)) + 10;
+    //generating random array between 20 to 150
+    
+    divs[i] = document.createElement("div")
+    
+    container.appendChild(divs[i])
+
     margin_size = 0.1;
-    divs[i].style = " margin:0% " + margin_size + "%; background-color:blue; width:" + (100 / array_size - (2 * margin_size)) + "%; height:" + (div_sizes[i]) + "%;";
+
+    divs[i].style = " margin:0% " + margin_size + "%; background-color:#40e0d0; width:" + (100 / array_size - (2 * margin_size)) + "%; height:" + (div_height[i]) + "%;";
 
 }
 
